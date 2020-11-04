812994b4cb200da19402c7a17b0beebc8507eb06
nachogg18 <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-11-04 11:32:32 -0300

    eliminado DomicilioController

--- src/main/java/com/example/persona/repositories/LocalidadRepository.java
+++ src/main/java/com/example/persona/repositories/LocalidadRepository.java
@@ -1,8 +1,6 @@
 package com.example.persona.repositories;
 
 import com.example.persona.entities.Localidad;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface LocalidadRepository extends BaseRepository<Localidad, Long> {
+public interface LocalidadRepository extends BaseRepository<Localidad, Long>{
 }
